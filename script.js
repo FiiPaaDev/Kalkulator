1f8b71d060a8539b7d3138c19af5650c277d1a4e
FiiPaaDev <81893789+FiiPaaDev@users.noreply.github.com>
2021-04-05 15:03:03 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButtons = document.querySelectorAll('[data-equals]');
+const deleteButtons = document.querySelectorAll('[data-delete]');
+const clearButtons = document.querySelectorAll('[data-clear]');
+const numberButtons = document.querySelectorAll('[data-number]');
+
